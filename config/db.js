b6c71828b943e6012c71dc7f537d8cce6a6da6e5
Sephyros <leo037br@hotmail.com>
2019-08-17 13:42:42 -0300

    New MongoURI

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://Sephyros:MyP%40%24%24w0rd@trymongo-s2iwg.mongodb.net/test'}
+    module.exports = {mongoURI: 'mongodb+srv://Sephyros:MyP%40%24%24w0rd@trymongo-s2iwg.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'} 
 }
