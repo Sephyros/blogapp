bfab64c7d595ab0a15f8a29f5370d1d5fbc11207
Sephyros <leo037br@hotmail.com>
2019-11-16 23:19:06 -0300

    Updated some dependecies

--- app.js
+++ app.js
@@ -1,127 +1,155 @@
 // Imports
-const express = require('express')
-const handlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-const app = express()
-const admin = require('./routes/admin')
-const user = require('./routes/user')
-const path = require('path')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const flash = require('connect-flash')
-require('./models/Post')
-const Post = mongoose.model('posts')
-require('./models/Category')
-const Category = mongoose.model('categories')
-const passport = require("passport")
-require('./config/auth')(passport)
-const db = require('./config/db')
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const app = express();
+const admin = require("./routes/admin");
+const user = require("./routes/user");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
+require("./models/Post");
+const Post = mongoose.model("posts");
+require("./models/Category");
+const Category = mongoose.model("categories");
+const passport = require("passport");
+require("./config/auth")(passport);
+const db = require("./config/db");
 
 // const i18n = require('./i18n')
 
 // Config
-    // i18n
-        // app.use(i18n)
-    // Session
-        app.use(session({
-            secret: 'mydearsecret',
-            resave: true,
-            saveUninitialized: true
-        }))
-        app.use(passport.initialize())
-        app.use(passport.session())
-        app.use(flash())
-    // Middleware
-        app.use((req, res, next) => {
-            res.locals.success_message = req.flash('success_message')
-            res.locals.error_message = req.flash('error_message')
-            res.locals.error = req.flash('error')
-            res.locals.user = req.user || null
-            next()
-        })
-    // Body Parser
-        app.use(bodyParser.urlencoded({extended: true}))
-        app.use(bodyParser.json())
-    // Handlebars
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
-    // Mongoose
-        mongoose.Promise = global.Promise
-        mongoose.connect(db.mongoURI, {useNewUrlParser: true}).then(() => {
-            console.log('Mongonected!')
-        }).catch((error) => {
-            console.log('OH NO DATABASE HAS EXPLODED! because of\n=======' + error + '=======')
-        })
-    // Public
-        app.use(express.static(path.join(__dirname, 'public')))
+// i18n
+// app.use(i18n)
+// Session
+app.use(
+  session({
+    secret: "mydearsecret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+// Middleware
+app.use((req, res, next) => {
+  res.locals.success_message = req.flash("success_message");
+  res.locals.error_message = req.flash("error_message");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Handlebars
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Mongoose
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Mongonected!");
+  })
+  .catch(error => {
+    console.log(
+      "OH NO DATABASE HAS EXPLODED! because of\n============================================\n" +
+        error +
+        "\n============================================"
+    );
+  });
+// Public
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-    app.get('/', (req, res) => {
-        Post.find().populate('category').populate('user').sort({date: 'DESC'}).then((posts) => {
-            res.render("index", {posts: posts})
-        }).catch((error) => {
-            req.flash('error_message', 'Whoops, Something gone wrong...' + error)
-            res.redirect('/404')
-        })
+app.get("/", (req, res) => {
+  Post.find()
+    .populate("category")
+    .populate("user")
+    .sort({ date: "DESC" })
+    .then(posts => {
+      res.render("index", { posts: posts });
     })
+    .catch(error => {
+      req.flash("error_message", "Whoops, Something gone wrong..." + error);
+      res.redirect("/404");
+    });
+});
 
-
-app.get('/post/:slug', (req, res) => {
-    Post.findOne({slug: req.params.slug}).populate('user').then((post) => {
-        if(post){
-                res.render('post/index', {post: post})
-            } else {
-                req.flash('error_message', 'Are you sure that post exists?')
-                res.redirect('/')
-            }
-    }).catch((error) => {
-        req.flash('error_msg', 'The world is changing... and something gone wrong! ')
-        res.redirect('/')
+app.get("/post/:slug", (req, res) => {
+  Post.findOne({ slug: req.params.slug })
+    .populate("user")
+    .then(post => {
+      if (post) {
+        res.render("post/index", { post: post });
+      } else {
+        req.flash("error_message", "Are you sure that post exists?");
+        res.redirect("/");
+      }
     })
-})
+    .catch(error => {
+      req.flash(
+        "error_msg",
+        "The world is changing... and something gone wrong! "
+      );
+      res.redirect("/");
+    });
+});
 
-    app.get('/404', (req, res) => {
-        res.send('Oh! you found a 404!')
-    })
+app.get("/404", (req, res) => {
+  res.send("Oh! you found a 404!");
+});
 
-    app.use('/admin', admin)
-    app.use('/user', user)
+app.use("/admin", admin);
+app.use("/user", user);
 
-    app.get('/categories', (req, res) => {
-        Category.find().then((categories) => {
-            res.render('categories/index', {categories: categories})
-        }).catch((error) => {
-            req.flash('error_message', 'Thank you for this beautiful ERROR!')
-            res.redirect('/')
-        })
+app.get("/categories", (req, res) => {
+  Category.find()
+    .then(categories => {
+      res.render("categories/index", { categories: categories });
     })
+    .catch(error => {
+      req.flash("error_message", "Thank you for this beautiful ERROR!");
+      res.redirect("/");
+    });
+});
 
-    app.get('/categories/:slug', (req, res) => {
-        Category.findOne({slug: req.params.slug}).then((category) => {
-            if(category){
-                Post.find({category: category._id}).then((posts) => {
-                    res.render('categories/posts', {posts: posts, category: category})
-                }).catch((error) => {
-                    req.flash('error_message', 'Error when listing posts...')
-                    res.redirect('/')
-                })
-            } else {
-                req.flash('error_message', 'This category doesn\'t exist')
-                res.redirect('/')
-            }
-        }).catch((error) => {
-            req.flash('error_message', 'Another one to the account!')
-            console.log(error)
-            res.redirect('/')
-        })
+app.get("/categories/:slug", (req, res) => {
+  Category.findOne({ slug: req.params.slug })
+    .then(category => {
+      if (category) {
+        Post.find({ category: category._id })
+          .then(posts => {
+            res.render("categories/posts", {
+              posts: posts,
+              category: category
+            });
+          })
+          .catch(error => {
+            req.flash("error_message", "Error when listing posts...");
+            res.redirect("/");
+          });
+      } else {
+        req.flash("error_message", "This category doesn't exist");
+        res.redirect("/");
+      }
     })
-    
-    app.get('posts', (req, res) => {
-        res.send('Posts')
-    })
+    .catch(error => {
+      req.flash("error_message", "Another one to the account!");
+      console.log(error);
+      res.redirect("/");
+    });
+});
+
+app.get("posts", (req, res) => {
+  res.send("Posts");
+});
 
 // Other
-    const PORT = process.env.PORT || 8080
-    app.listen(PORT, () => {
-        console.log('Server Up!')
-    })
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Server Up!");
+});
--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
     module.exports = {mongoURI: 'mongodb+srv://admin:SuperPassword2019@mysecretdata-s2iwg.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
-    module.exports = {mongoURI: 'mongodb://localhost/blogapp'} 
+    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
